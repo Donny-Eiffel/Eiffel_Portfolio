db345a14053af6b3fea1577fd83c2283745edfed
Donny-Eiffel <donnyeiffel@gmail.com>
2025-06-23 16:08:07 +0200

    updated files

--- script.js
+++ script.js
@@ -1,252 +1,239 @@
 const mobileMenu = document.getElementById('mobile_menu');
-mobileMenu.addEventListener("click",()=>{
-const desktopMenu = document.getElementById("desktop_menu");
-mobileMenu.classList.toggle("active");
-desktopMenu.classList.toggle("active");
+mobileMenu.addEventListener('click', () => {
+  const desktopMenu = document.getElementById('desktop_menu');
+  mobileMenu.classList.toggle('active');
+  desktopMenu.classList.toggle('active');
 });
 
-
-//object to store card properties
+// object to store card properties
 const projectCards = [
-    {
-        image: "./assets/images/icon-languages.svg",
-        title: "multi-post stories Gain+Glory",
-        technologies: ['Ruby on Rails', 'css' , 'javaScript' , 'HTML'],
-        description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
-        githunLink: "#",
-        liverLink:"#",
-        link:"#"
-    },
+  {
+    image: './assets/images/icon-languages.svg',
+    title: 'multi-post stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'css', 'javaScript', 'HTML'],
+    description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
+    githunLink: '#',
+    liverLink: '#',
+    link: '#',
+  },
 
-    {
-        image: "./assets/images/icon-languages.svg",
-        title: "multi-post stories Gain+Glory",
-        technologies: ['Ruby on Rails', 'css' , 'javaScript' , 'HTML'],
-        description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
-        githunLink: "#",
-        liverLink:"#",
-        link:"#"
-    },
+  {
+    image: './assets/images/icon-languages.svg',
+    title: 'multi-post stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'css', 'javaScript', 'HTML'],
+    description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
+    githunLink: '#',
+    liverLink: '#',
+    link: '#',
+  },
 
-    {
-        image: "./assets/images/icon-languages.svg",
-        title: "multi-post stories Gain+Glory",
-        technologies: ['Ruby on Rails', 'css' , 'javaScript' , 'HTML'],
-        description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
-        githunLink: "#",
-        liverLink:"#",
-        link:"#"
-    },
+  {
+    image: './assets/images/icon-languages.svg',
+    title: 'multi-post stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'css', 'javaScript', 'HTML'],
+    description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
+    githunLink: '#',
+    liverLink: '#',
+    link: '#',
+  },
 
-    {
-        image: "./assets/images/icon-languages.svg",
-        title: "multi-post stories Gain+Glory",
-        technologies: ['Ruby on Rails', 'css' , 'javaScript' , 'HTML'],
-        description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
-        githunLink: "#",
-        liverLink:"#",
-        link:"#"
-    },
+  {
+    image: './assets/images/icon-languages.svg',
+    title: 'multi-post stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'css', 'javaScript', 'HTML'],
+    description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
+    githunLink: '#',
+    liverLink: '#',
+    link: '#',
+  },
 
-    {
-        image: "./assets/images/icon-languages.svg",
-        title: "multi-post stories Gain+Glory",
-        technologies: ['Ruby on Rails', 'css' , 'javaScript' , 'HTML'],
-        description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
-        githunLink: "#",
-        liverLink:"#",
-        link:"#"
-    },
+  {
+    image: './assets/images/icon-languages.svg',
+    title: 'multi-post stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'css', 'javaScript', 'HTML'],
+    description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
+    githunLink: '#',
+    liverLink: '#',
+    link: '#',
+  },
 
-    {
-        image: "./assets/images/icon-languages.svg",
-        title: "multi-post stories Gain+Glory",
-        technologies: ['Ruby on Rails', 'css' , 'javaScript' , 'HTML'],
-        description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
-        githunLink: "#",
-        liverLink:"#",
-        link:"#"
-    },
+  {
+    image: './assets/images/icon-languages.svg',
+    title: 'multi-post stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'css', 'javaScript', 'HTML'],
+    description: "A plateform to share and celebrate stories through multi posts. lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relealorem ipsum is simply dummy text of the printing and typesetting industry.",
+    githunLink: '#',
+    liverLink: '#',
+    link: '#',
+  },
 ];
-
 
 const projectCardsTwo = [
-    {
-        image: "./assets/images/icon-languages.svg",
-        title: "languages",
-        languages: ['javaScript', 'Ruby', 'Html','css'],
-    },
+  {
+    image: './assets/images/icon-languages.svg',
+    title: 'languages',
+    languages: ['javaScript', 'Ruby', 'Html', 'css'],
+  },
 
-    {
-        image: "./assets/images/icon-frameworks (1).svg",
-        title: "languages",
-        languages: ['React.js', 'Ruby', 'RSpect','Capybara','selenium'],
-    },
+  {
+    image: './assets/images/icon-frameworks (1).svg',
+    title: 'languages',
+    languages: ['React.js', 'Ruby', 'RSpect', 'Capybara', 'selenium'],
+  },
 
-    {
-        image: "./assets/images/icon-skills.svg",
-        title: "languages",
-        languages: ['Database Management', 'Version Control', 'Cli','Web Development', 'API Design'],
-    },
+  {
+    image: './assets/images/icon-skills.svg',
+    title: 'languages',
+    languages: ['Database Management', 'Version Control', 'Cli', 'Web Development', 'API Design'],
+  },
 ];
 
-console.log('hi',projectCards);
+// console.log('hi', projectCards);
 
-//define function 
-function openModal(project) {
-    const modal = document.createElement("div");
-    modal.id = 'projectModal';  // Correct ID
-    modal.classList.add('modal');
-
-    const modalContent = document.createElement("div");
-    modalContent.classList.add('modalcontent');
-    modal.appendChild(modalContent);  // Append modalContent to modal
-
-    const closeButton = document.createElement("span");
-    closeButton.classList.add("closebutton");
-    closeButton.innerHTML = "&times;";
-    closeButton.addEventListener("click", closeModal);  // Call closeModal correctly
-    modalContent.appendChild(closeButton);
-
-    const modalImage = document.createElement("img");
-    modalImage.classList.add("modalImage");
-    modalImage.src = project.image;
-    modalImage.alt = project.title;
-
-  modalContent.appendChild(modalImage)
-    const modalTitle = document.createElement("h3");
-    modalTitle.textContent = project.title; //display title 
-
-    
-
-const list = document.createElement("ul");
-list.id = "modaltechnologies";
-project.technologies.forEach(tech =>{
-const listItem = document.createElement("li");
-listItem.innerHTML = tech;
-list.appendChild(listItem);})
-
-
-
-const modalDescription = document.createElement("p");
-modalDescription.textContent=project.description
-modalContent.append(modalTitle ,list, modalDescription)
-
-
-document.body.appendChild(modal);
+function closeModal() {
+  const modal = document.getElementById('projectModal');
+  if (modal) {
+    modal.remove();
+    // window.removeEventListener('click', outsideClick);
+  }
 }
 
+// define function
+function openModal(project) {
+  const modal = document.createElement('div');
+  modal.id = 'projectModal'; // Correct ID
+  modal.classList.add('modal');
 
- function closeModal(){
-      const modal = document.getElementById("projectModal");
-      if (modal){
-        modal.remove()
-        window.removeEventListener("click",  outsideClick);
-      }
-    } 
-    function outsideClick(event) {
-      if (event.target === event.currentTarget) {
-        closeModal();
-      }
-    }
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modalcontent');
+  modal.appendChild(modalContent); // Append modalContent to modal
+
+  const closeButton = document.createElement('span');
+  closeButton.classList.add('closebutton');
+  closeButton.innerHTML = '&times;';
+  closeButton.addEventListener('click', closeModal); // Call closeModal correctly
+  modalContent.appendChild(closeButton);
+
+  const modalImage = document.createElement('img');
+  modalImage.classList.add('modalImage');
+  modalImage.src = project.image;
+  modalImage.alt = project.title;
+
+  modalContent.appendChild(modalImage);
+  const modalTitle = document.createElement('h3');
+  modalTitle.textContent = project.title; // display title
+
+  const list = document.createElement('ul');
+  list.id = 'modaltechnologies';
+  project.technologies.forEach((tech) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = tech;
+    list.appendChild(listItem);
+  });
+
+  const modalDescription = document.createElement('p');
+  modalDescription.textContent = project.description;
+  modalContent.append(modalTitle, list, modalDescription);
+
+  document.body.appendChild(modal);
+}
+
+// function outsideClick(event) {
+//   if (event.target === event.currentTarget) {
+//     closeModal();
+//   }
+// }
 
 projectCards.forEach((project) => {
-    const projectCard = document.getElementById('cards');
-    const firstDiv = document.createElement('div');
-    firstDiv.classList.add('card');
+  const projectCard = document.getElementById('cards');
+  const firstDiv = document.createElement('div');
+  firstDiv.classList.add('card');
 
-    const cardImage = document.createElement('div');
-    cardImage.classList.add('card-image');
+  const cardImage = document.createElement('div');
+  cardImage.classList.add('card-image');
 
-    const img = document.createElement('img');
-    img.src = project.image;
-    img.alt = project.title;
+  const img = document.createElement('img');
+  img.src = project.image;
+  img.alt = project.title;
 
-    cardImage.appendChild(img);
+  cardImage.appendChild(img);
 
-    const secondDiv = document.createElement('div');
-    secondDiv.classList.add('card-details');
+  const secondDiv = document.createElement('div');
+  secondDiv.classList.add('card-details');
 
-    const cardTitle = document.createElement('div');
-    cardTitle.classList.add('title');
+  const cardTitle = document.createElement('div');
+  cardTitle.classList.add('title');
 
-    const headingOne = document.createElement('h3');
-    headingOne.innerHTML = project.title;
+  const headingOne = document.createElement('h3');
+  headingOne.innerHTML = project.title;
 
-    const cardLanguage = document.createElement('div');
-    cardLanguage.classList.add('languages');
+  const cardLanguage = document.createElement('div');
+  cardLanguage.classList.add('languages');
 
-    const listItems = document.createElement('ul');
+  const listItems = document.createElement('ul');
 
-    project.technologies.forEach((list) => {
-        const li = document.createElement('li');
-        li.innerHTML = list;
-        listItems.appendChild(li);
-    });
+  project.technologies.forEach((list) => {
+    const li = document.createElement('li');
+    li.innerHTML = list;
+    listItems.appendChild(li);
+  });
 
-    const cardBtn = document.createElement('div');
-    cardBtn.classList.add('btn');
+  const cardBtn = document.createElement('div');
+  cardBtn.classList.add('btn');
 
-    const btn = document.createElement('button');
-    btn.type = 'button';  // Changed to 'button' to prevent form submission
-    btn.innerHTML = 'See Project';
+  const btn = document.createElement('button');
+  btn.type = 'button'; // Changed to 'button' to prevent form submission
+  btn.innerHTML = 'See Project';
 
-    btn.addEventListener('click', () => openModal(project));  // Pass a function reference
+  btn.addEventListener('click', () => openModal(project)); // Pass a function reference
 
-    cardBtn.appendChild(btn);
-    cardLanguage.appendChild(listItems);
-    cardTitle.appendChild(headingOne);
+  cardBtn.appendChild(btn);
+  cardLanguage.appendChild(listItems);
+  cardTitle.appendChild(headingOne);
 
-    secondDiv.append(cardTitle, cardLanguage, cardBtn);
-    firstDiv.append(cardImage, secondDiv);
-    projectCard.appendChild(firstDiv);
-    console.log(project);
+  secondDiv.append(cardTitle, cardLanguage, cardBtn);
+  firstDiv.append(cardImage, secondDiv);
+  projectCard.appendChild(firstDiv);
+//   console.log(project);
 });
 
+// console.log('hi', projectCardsTwo);
+projectCardsTwo.forEach((tech) => {
+  const techCard = document.getElementById('technologies');
+  const divOne = document.createElement('div');
+  divOne.classList.add('tech-card');
 
+  const techImage = document.createElement('div');
+  techImage.classList.add('tech-image');
 
-console.log('hi',projectCardsTwo);
-projectCardsTwo.forEach((tech)=>{
+  const img2 = document.createElement('img');
+  img2.src = tech.image;
+  img2.alt = tech.title;
 
-const techCard = document.getElementById('technologies')
-const divOne = document.createElement('div');
-    divOne.classList.add('tech-card');
+  techImage.appendChild(img2);
 
-const techImage = document.createElement('div');
-    techImage.classList.add('tech-image');
+  const techTitle = document.createElement('div');
+  techTitle.classList.add('tech-title');
 
-const img2 = document.createElement('img');
-    img2.src = tech.image;
-    img2.alt = tech.title;
+  const headingTwo = document.createElement('h3');
+  headingTwo.innerHTML = tech.title;
+  techTitle.appendChild(headingTwo);
 
-    techImage.appendChild(img2);
+  const techLanguage = document.createElement('div');
+  techLanguage.classList.add('tech-language');
 
-const techTitle = document.createElement('div');
-    techTitle.classList.add('tech-title');
+  const listItemsTwo = document.createElement('ul');
 
-const headingTwo = document.createElement('h3');
-    headingTwo.innerHTML = tech.title;
-    techTitle.appendChild(headingTwo);
+  tech.languages.forEach((list) => {
+    const li2 = document.createElement('li');
+    li2.innerHTML = list;
+    listItemsTwo.appendChild(li2);
+  });
 
-const techLanguage = document.createElement('div');
-    techLanguage.classList.add('tech-language');
+  techLanguage.appendChild(listItemsTwo);
 
-const listItemsTwo = document.createElement('ul');
+  divOne.append(techImage, techTitle, techLanguage);
 
-    tech.languages.forEach((list)=>{
-        const li2 = document.createElement('li');
-        li2.innerHTML = list;
-        listItemsTwo.appendChild(li2);
-    });
-
-
-    techLanguage.appendChild(listItemsTwo);
-
-
-
-    divOne.append(techImage,techTitle,techLanguage);
-    
-    
-    techCard.appendChild(divOne)
-    console.log(tech)
-})
+  techCard.appendChild(divOne);
+//   console.log(tech);
+});
